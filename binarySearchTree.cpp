9d781db3eddd82242b4907f17eb148209a20cca6
akmalropip <akmalropip@gmail.com>
2026-06-12 15:07:01 +0700

    memberi akses modifier serta mendeklarasi variabel dan object node

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -4,4 +4,8 @@ using namespace std;
 //membuat class Node
 class Node {
 
-    
+    //memberi akses modifier serta mendeklarasi variabel dan object node
+    public :
+    string info;
+    Node* leftchild;
+    Node* rightchild;
